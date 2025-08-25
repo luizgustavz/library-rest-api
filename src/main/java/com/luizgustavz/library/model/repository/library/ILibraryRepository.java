3f9a2f386402d2ae5a1511e6ea57c67d574e9559
luizgustavz <luiz.gus.ms@gmail.com>
2025-08-25 19:16:51 -0300

    refactor: change the repository layer to inside the model folder for better visualization

--- src/main/java/com/luizgustavz/library/model/repository/library/ILibraryRepository.java
+++ src/main/java/com/luizgustavz/library/model/repository/library/ILibraryRepository.java
@@ -1,4 +1,4 @@
-package com.luizgustavz.library.repository.library;
+package com.luizgustavz.library.model.repository.library;
 
 import com.luizgustavz.library.model.entities.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
